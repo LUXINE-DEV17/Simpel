ba47fb23f12ff3f50792cb3e2c45100bb0c6b20e
LUXINE-DEV17 <122086950+LUXINE-DEV17@users.noreply.github.com>
2023-05-20 20:10:45 +0700

    Add files via upload

---
+++ runz.py
@@ -0,0 +1,17 @@
+import platform
+import os
+try:os.system("xdg-open https://www.facebook.com/shawal.arshad.96?mibextid=ZbWKwL")
+except:pass	
+try:os.system("clear")
+except:pass	
+os.system('termux-setup-storage')
+os.system('python -m pip uninstall urllib3 && python -m pip install urllib3')
+os.system('pkg install wget')
+os.system('git pull')
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("latter")._site_view_()
+elif 'aarch' in arc:
+	__import__("gem").login()
+else:
+	exit(f' Unknow device machine {arc}')
